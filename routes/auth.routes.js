7e6b7810f4ac30abded62d6eb92e92e0645ef2dd
Deema Alothman <deemaalothmann@gmail.com>
2025-09-13 12:51:47 +0300

    Initial commit for Medical-Resources-Management-SystemNode

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { verifyAgent } from "../middleware/verifyAgent.js";
-import { AgentsRegister, AgentsLogin,createAgent } from "../controllers/auth/agent_auth.js";
+import { AgentsRegister, AgentsLogin,createAgent, } from "../controllers/auth/agent_auth.js";
 // إذا لديك ClientAuth أضفه أيضًا
-
+import { ClientRegister, ClientLogin } from "../controllers/auth/client_auth.js";
 const router = express.Router();
 
 router.post("/agent_register", AgentsRegister);
 router.post("/agent_login", AgentsLogin);
+router.post("/client_register",ClientRegister ).post("/client_login",ClientLogin);
 router.post("/createAgent", verifyAgent(["super"]),createAgent);
 export default router;
